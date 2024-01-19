83c08defbbcb1b9a86cc5dfacb8e24ff962376fb
Vansh sharma <91759509+vanshsharma5503@users.noreply.github.com>
2024-01-19 10:07:43 +0530

    Add files via upload

---
+++ LoginAndSignin/LoginAndSignin/LoginAndSigninApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAndSigninApp.swift
+//  LoginAndSignin
+//
+//  Created by user1 on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginAndSigninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
